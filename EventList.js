5344f109979dbe7b08e829fd4137297ffb0141bc
MartinOpatovsky <56114781+MartinOpatovsky@users.noreply.github.com>
2021-04-04 16:30:45 +0200

    background
    Co-Authored-By: Lucia Mandová <49123897+lucikluc@users.noreply.github.com>

--- EventList.js
+++ EventList.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+import { View } from 'react-native';
+//import LinearGradient from 'react-native-linear-gradient';
+import { LinearGradient } from 'expo-linear-gradient';
+//import { LinearGradient } from 'expo';
 
 class EventList extends Component {
     render(){
         return (
-            <Text>Sejusky</Text>
+            //<Text>Sejusky</Text>
+            <View style={styles.container}>
+                <LinearGradient
+                    colors={['#5EF9D4', 'white']}
+                    style={styles.background}>
+                </LinearGradient>
+            </View>
         );
     }
 }
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#5EF9D4',
+    },
+    background: {
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      top: 0,
+      height: 1000,
+    },
+    button: {
+      padding: 15,
+      alignItems: 'center',
+      borderRadius: 5,
+    },
+    text: {
+      backgroundColor: 'transparent',
+      fontSize: 15,
+      color: '#fff',
+    },
+  });
 
 export default EventList;
