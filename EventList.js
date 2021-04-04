54e15a54d7f62db62a061bdb6565209ed02fe9a9
MartinOpatovsky <56114781+MartinOpatovsky@users.noreply.github.com>
2021-04-04 15:31:41 +0200

    First steps
    Co-Authored-By: Lucia Mandová <49123897+lucikluc@users.noreply.github.com>

---
+++ EventList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class EventList extends Component {
+    render(){
+        return (
+            <Text>Sejusky</Text>
+        );
+    }
+}
+
+export default EventList;
