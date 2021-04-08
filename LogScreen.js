5be4b773e8f257f7a00a4d03b5f52e95a276b4b8
MartinOpatovsky <56114781+MartinOpatovsky@users.noreply.github.com>
2021-04-08 15:58:19 +0200

    login
    Co-Authored-By: Lucia Mandová <49123897+lucikluc@users.noreply.github.com>

--- LogScreen.js
+++ LogScreen.js
@@ -88,6 +88,7 @@ class LogScreen extends React.Component{
       </Formik> 
       );      
     }
+<<<<<<< Updated upstream
   login(values){
     const url = 'https://mtaa-pets.herokuapp.com/user/login/';
     const options = {
@@ -95,6 +96,17 @@ class LogScreen extends React.Component{
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'text/plain'
+=======
+
+  prihlas(){
+    const url = 'http://127.0.0.1:8000/pets/?breed=Jašterica krátkohlavá';
+    const options = {
+      method: 'GET',
+      //mode: 'no-cors',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json;charset=UTF-8'
+>>>>>>> Stashed changes
       },
       body: JSON.stringify({
         'nick/email': values['emailnick'],
@@ -102,6 +114,7 @@ class LogScreen extends React.Component{
       })
     };
     fetch(url, options)
+<<<<<<< Updated upstream
       .then(ApiUtils.checkStatus)
       .then(response => response.json())
       .catch(e => e)
@@ -109,6 +122,16 @@ class LogScreen extends React.Component{
     }
 }
 
+=======
+      .then(response => response.json())
+      .then(jsondata => console.log(jsondata))
+}
+}
+
+//.then(response => {
+  //console.log(response);
+//});
+>>>>>>> Stashed changes
 
 const styles = StyleSheet.create({
   container:{
